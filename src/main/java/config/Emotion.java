dfca8d34740b79a46d04a3fef4a00622230373dc
ivajefremova <jefremovaiva17@gmail.com>
2026-05-28 17:31:18 +0200

    emotion enum done

--- src/main/java/config/Emotion.java
+++ src/main/java/config/Emotion.java
@@ -6,7 +6,23 @@ public enum Emotion {
     ANGRY,
     DISGUST,
     FEAR,
-    NEUTRAL,
+    NEUTRAL;
 
+    public String getLabel() {
+        return this.name();
+    }
+    //returns the index
+    public int findIndex() {
+        return this.ordinal();
+    }
+
+    //takes index and finds its associated emotion
+    public static Emotion findEmotion(int index) {
+        Emotion[] values = Emotion.values();
+        if (index < 0 || index >= values.length) {
+            throw new IllegalArgumentException("invalid emotion index: " + index +". only 0 to 5 allowed");
+        }
+        return values[index];
+    }
 
 }
