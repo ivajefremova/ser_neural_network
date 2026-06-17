6774e32bfd021e8f4aa7f23705cb08b4b265d0b5
ivajefremova <jefremovaiva17@gmail.com>
2026-06-17 23:04:35 +0200

    Ignore .wav files, add normalizer, add comments to audio classes

--- src/main/java/audio/DatasetSplitter.java
+++ src/main/java/audio/DatasetSplitter.java
@@ -1,30 +1,37 @@
 package audio;
 
-//Splits the data into two sets (training and testing) in 80:20 propotion, in a way that the same voice actor will never appear in both
+//splits the data into two sets (training and testing) in 80:20 proportion, and returns those
+//in a way that the same voice actor will never appear in both
 
 import java.io.File;
 import java.util.*;
+//The * is a wildcard import — it imports everything from the java.util package at once instead of listing separately.
+//  import java.util.List;
+//  import java.util.ArrayList;
+//  import java.util.Set;
+//  import java.util.HashSet;
 
 public class DatasetSplitter {
     public static List<List<LabeledAudio>> split(List<LabeledAudio> dataset){
 
-        List<String> actorList = new ArrayList<>();
+        List<String> actorList = new ArrayList<>();     //list of strings of each actor
         for (LabeledAudio audio : dataset) {
-            String actorId = new File(audio.getFilePath()).getName().split("_")[0];
+            String actorId = new File(audio.getFilePath()).getName().split("_")[0];    //the numbers at the start are actor id
             if (!actorList.contains(actorId)) {
                 actorList.add(actorId);
             }
         }
-        Collections.shuffle(actorList);
+        Collections.shuffle(actorList);    //method from util* to mix the actor list
 
-        int splitPoint = (int)(actorList.size() * 0.8);
+        int splitPoint = (int)(actorList.size() * 0.8);             //the split 80 for training 20 for testing
 
-        Set<String> trainActors = new HashSet<>(actorList.subList(0, splitPoint));
+        Set<String> trainActors = new HashSet<>(actorList.subList(0, splitPoint));      //sublist and set are from list interface, sublist gets the sublist until the spliypoint
         Set<String> testActors  = new HashSet<>(actorList.subList(splitPoint, actorList.size()));
 
         List<LabeledAudio> trainingList = new ArrayList<>();
         List<LabeledAudio> testList = new ArrayList<>();
 
+        //add some actors in training list and some in test list
         for (LabeledAudio a : dataset) {
             String actorId = new File(a.getFilePath()).getName().split("_")[0];
             if (trainActors.contains(actorId)) {
@@ -34,6 +41,6 @@ public class DatasetSplitter {
             }
         }
 
-        return Arrays.asList(trainingList, testList);
+        return Arrays.asList(trainingList, testList); //list of 2 sets training and test list
     }
 }
--- src/main/java/audio/LabeledAudio.java
+++ src/main/java/audio/LabeledAudio.java
@@ -1,17 +1,19 @@
 package audio;
 import config.Emotion;
 
-//Creates an object that stores an emotion and the path to the audio file
+//creates an object that stores an emotion and the path to the audio file
 
 public class LabeledAudio {
     private String filePath;
     private Emotion label;
 
+    //constructor
     public LabeledAudio(String filePath, Emotion label){
         this.filePath = filePath;
         this.label = label;
     }
 
+    //getters
     public String getFilePath(){
         return filePath;
     }
